b49b2997704c2c856741884b5bda6ea5912ed2c7
Klemen <klemen.nedelko2@gmail.com>
2023-03-08 01:27:03 +0100

    added weather function

--- src/components/Wailton/Wailton.js
+++ src/components/Wailton/Wailton.js
@@ -1,13 +1,48 @@
 import { Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../Layout/Layout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCloudSunRain } from '@fortawesome/free-solid-svg-icons'
+import Search from './search'
+import CurrentWeather from './current-weather/currentWeather'
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api'
+import Forecast from './forecast/Forecast'
 
 const Wailton = () => {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+  console.log(forecast);
   return (
     <Layout>
-        <Typography  variant='h6'>Weather visualization  <FontAwesomeIcon icon={faCloudSunRain} /></Typography> 
+      <Typography variant='h6'>Weather visualization  <FontAwesomeIcon icon={faCloudSunRain} /></Typography>
+      <Search
+        onSearchChange={handleOnSearchChange}
+      />
+      {currentWeather ? <CurrentWeather
+        data={currentWeather}
+      /> :
+        " "}
+        {forecast? <Forecast data={forecast}/>:""}
     </Layout>
   )
 }
---
+++ src/components/Wailton/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a7570dec09msh40dd60d19359413p1d6f37jsne4408cb303c1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "06ab1b1928199f70fe686bb08848323b";
